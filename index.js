c97a1d0d931a5384028ec12df07016c0146dcb00
Xiaoyi Liu <lxy878@gmail.com>
2020-08-30 17:28:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(array, target){
+  return array.filter(driver => driver.toLowerCase()===target.toLowerCase());
+}
+
+function fuzzyMatch(array, target){
+  return array.filter(driver => driver.slice(0, 2) === target);
+}
+
+function matchName(array, target){
+  return array.filter(driver => driver.name === target);
+}
